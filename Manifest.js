a1a90745d78109fd0e184dd1317d489fb3cdc87e
rikhirajbhardwaj250-sys <rikhirajbhardwaj250@gmail.com>
2026-09-22 20:30:56 +0530

    Create Manifest.js

---
+++ Manifest.js
@@ -0,0 +1,17 @@
+{
+  "name": "Feasto - Food Delivery",
+  "short_name": "Feasto",
+  "start_url": "./customer.html",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#EA580C",
+  "orientation": "portrait-primary",
+  "icons": [
+    {
+      "src": "https://i.supaimg.com/0102605e-3d7b-40fe-b036-ff43cacb268e/02c8568f-70bc-4207-aeb1-887bbf212b73.png",
+      "sizes": "192x192 512x512",
+      "type": "image/png",
+      "purpose": "any maskable"
+    }
+  ]
+}
